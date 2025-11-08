cc01205a346206b758e98ea21a505f056f16126d
arajpk <arajpk.mail@gmail.com>
2025-11-08 08:00:29 +0000

    utils_header

---
+++ include/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H_INCLUDED
+#define UTILS_H_INCLUDED
+#include <stdlib.h>
+#include <time.h>
+
+void currentTimestamp(char *out, size_t len);
+int simulateLDR();
+int simulateTemperature();
+int simulatePresence();
+
+
+
+#endif // UTILS_H_INCLUDED
